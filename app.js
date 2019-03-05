2573903fe4baad2087d101c234cf46d6ab0b870e
Mustafa Elimam <mjkam88@gmail.com>
2019-03-05 00:32:04 -0800

    app.js file added

---
+++ app.js
@@ -0,0 +1,23 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var routeRouter = require('./routes/route2');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+// app.use('/', usersRouter);
+app.use('/route2', routeRouter);
+
+
+module.exports = app;
